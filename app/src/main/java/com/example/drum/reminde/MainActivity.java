b0cc9c8495f18d414797877dcd844af211c0359f
Den Gerashenko <straus94@gmail.com>
2015-11-21 12:05:24 +0200

    Create NavigationDrawer. Fix styles and layout.

--- app/src/main/java/com/example/drum/reminde/MainActivity.java
+++ app/src/main/java/com/example/drum/reminde/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.drum.reminde;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 /**
  * Created by drum on 19.11.2015.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
