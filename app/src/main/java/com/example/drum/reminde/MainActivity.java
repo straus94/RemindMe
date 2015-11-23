65d482960b600e77b7e828fad46b56e2b0c5735c
Den Gerashenko <straus94@gmail.com>
2015-11-23 11:57:38 +0200

    Create menu, navigation drawer and navigation header/

--- app/src/main/java/com/example/drum/reminde/MainActivity.java
+++ app/src/main/java/com/example/drum/reminde/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.drum.reminde;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -10,15 +11,20 @@ import android.view.MenuItem;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -32,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
     }
 }
